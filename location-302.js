71cdb0c71ee3447d954f8de7bc15e4d2bdd3fb7e
dmitrik <dmitrik@ironsrc.com>
2015-05-21 09:25:31 +0300

    substring token

--- location-302.js
+++ location-302.js
@@ -4,8 +4,9 @@ var crypto = require('crypto');
 module.exports = function(id, secret, url) {
 	var serviceUrl = "http://302-location.com";
 
-	var params = {"id":id, "url":url};
-	params["token"] = crypto.createHash('sha256').update(secret + querystring.stringify(params)).digest('hex');
+	var params = {"i":id, "u":url};
+	params["t"] = crypto.createHash('sha256').update(secret + querystring.stringify(params)).digest('hex');
+	params["t"] = params["t"].substring(0, 4);
 
 	return serviceUrl+"/?"+querystring.stringify(params);
 }

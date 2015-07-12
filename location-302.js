e08750bca9628e60372e3c5f56a88e09b38e55e5
dmitrik <dmitrik@ironsrc.com>
2015-07-12 18:31:30 +0300

    remove id from token

--- location-302.js
+++ location-302.js
@@ -4,7 +4,7 @@ var crypto = require('crypto');
 module.exports = function(id, secret, url) {
 	var serviceUrl = "http://302-location.com";
 
-	var token = crypto.createHash('sha256').update(secret+id+url).digest('hex');
+	var token = crypto.createHash('sha256').update(secret+url).digest('hex');
 	token = token.substring(0, 4);
 
 	return serviceUrl+"/?"+querystring.stringify({"i":id, "u":url, "t":token});
